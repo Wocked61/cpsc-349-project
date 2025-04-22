208b12ed9785670a2ba3b616a2c6ee501df53498
Wocked61 <DylanP@csu.fullerton.edu>
2025-04-22 14:33:20 -0700

    established the map while making the base for the home

---
+++ 349 Project/src/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Home() {
+
+
+return (
+    <div className="Home">
+        <Header />
+        <main>
+            <h1>Welcome to the Timezone/Weather App</h1>
+            <p>This app allows you to view timezones and weather information.</p>
+            <p>Use the navigation bar to explore different features.</p>
+        </main>
+            <Footer />
+        </div>
+);
+}
+
+
+
+export default Home;
--- 349 Project/src/main.jsx
+++ 349 Project/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Home.jsx';
+// import Home from './Home.jsx';
 // import App from './App.jsx';
 // import Timezone from './Timezone.jsx';
-// import Map from './Map.jsx';
+import Map from './Map.jsx';
 import './index.css';
     // <Timezone />
     // <Map />
@@ -11,6 +11,6 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <Home />
+    <Map />
   </React.StrictMode>
 );
